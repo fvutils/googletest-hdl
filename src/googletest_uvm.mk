87a2e34736209dc64b1ed9cd1c6ea26c17462f1c
mballance-sf <matt.ballance@gmail.com>
2018-01-04 09:32:43 -0800

    Rename

---
+++ src/googletest_uvm.mk
@@ -0,0 +1,33 @@
+
+GOOGLETEST_UVM_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+ifneq (1,$(RULES))
+
+CXXFLAGS += -I$(GOOGLETEST_UVM_DIR)
+CXXFLAGS += -I$(GOOGLETEST_UVM_DIR)/../googletest/googletest/include
+CXXFLAGS += -I$(GOOGLETEST_UVM_DIR)/../googletest/googletest
+CXXFLAGS += -DGTEST_HAS_PTHREAD=0
+
+GOOGLETEST_UVM_OBJ = \
+	gtest-all.o \
+	googletest_uvm.o
+	
+GOOGLETEST_OBJ = \
+	gtest-all.o 
+	
+	
+SRC_DIRS += $(GOOGLETEST_UVM_DIR)
+SRC_DIRS += $(GOOGLETEST_UVM_DIR)/../googletest/googletest/src
+
+DPI_OBJS_LIBS += googletest_uvm_dpi.o
+
+else
+
+googletest_uvm_dpi.o : $(GOOGLETEST_UVM_OBJ)
+	$(LD) -r -o $@ $^
+	
+googletest.o : $(GOOGLETEST_OBJ)
+	$(LD) -r -o $@ $^
+
+endif
+
